4233e6a142b8a048d068411315fa66f55a8bdd3a
ProfRiski <99380737+ProfRiski@users.noreply.github.com>
2022-11-22 16:52:36 +0700

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -21,7 +21,7 @@ const seo = {
 const greeting = {
   title: "Athif",
   logo_name: "Athif",
-  nickname: "@athif3017",
+  nickname: "athif3017",
   subTitle:
     "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
   resumeLink:
