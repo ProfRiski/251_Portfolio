8c997bb12c04e75bb55844c9df7abbbf37b85ad3
ProfRiski <99380737+ProfRiski@users.noreply.github.com>
2022-11-22 16:51:50 +0700

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -9,7 +9,7 @@ const settings = {
 const seo = {
   title: "Athif's Portfolio",
   description:
-    "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
+    "A passionate individual who always learn about coding without giving up.",
   og: {
     title: "Athif's Portfolio",
     type: "website",
