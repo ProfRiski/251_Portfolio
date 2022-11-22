d28a79c479dcf78ab26055a3556d6f10fb4b6b2a
ProfRiski <99380737+ProfRiski@users.noreply.github.com>
2022-11-22 19:06:25 +0700

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -159,6 +159,16 @@ const degrees = {
   degrees: [
     {
       title: "Privacy",
+      subtitle: "",
+      logo_path: '",
+      alt_name: "",
+      duration: "",
+      descriptions: [
+        "⚡ I have studied basic software engineering subjects like DS, Algorithms, DBMS, OS, CA, AI etc.",
+        "⚡ Apart from this, I have done courses on Deep Learning, Data Science, Cloud Computing and Full Stack Development.",
+        "⚡ I was selected for Merit cum Means Scholarship which is given to top 10% of students in college. I have received award from respected director for consistently best performance in academics.",
+      ],
+      website_link: "",
     },
   ],
 };
